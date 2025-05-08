a893b53649f3c963cd1c794ed6c35502bf618e61
gammer <145098169+akbarAhmatov@users.noreply.github.com>
2025-05-08 08:43:07 +0500

    add files

---
+++ src/calculator/Main.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("5 + 3 = " + calc.add(5, 3));
+        System.out.println("10 - 4 = " + calc.subtract(10, 4));
+        System.out.println("6 * 7 = " + calc.multiply(6, 7));
+        System.out.println("20 / 5 = " + calc.divide(20, 5));
+    }
+}
